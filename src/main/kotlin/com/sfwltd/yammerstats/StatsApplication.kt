2b490aab485d912e56ad207c39dc8017003651b2
Adam Londero <alondero@gmail.com>
2016-04-25 18:42:54 +0530

    Adding Main Class

---
+++ src/main/kotlin/com/sfwltd/yammerstats/StatsApplication.kt
@@ -0,0 +1,12 @@
+package com.sfwltd.yammerstats
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+open class StatsApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(StatsApplication::class, *args);
+}
+
